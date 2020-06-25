55e2769680d52d94b49a911e932198f393527f3e
ManaN Patel <mananpatel320@gmail.com>
2020-06-25 11:22:35 +0530

    made changes to the dashboard and track shipment page

---
+++ client/src/components/shipmentTrack/Chart2.js
@@ -0,0 +1,130 @@
+import * as React from 'react';
+import Paper from '@material-ui/core/Paper';
+import {
+  Chart,
+  ArgumentAxis,
+  ValueAxis,
+  LineSeries,
+  ZoomAndPan,
+  Title,
+  Legend
+} from '@devexpress/dx-react-chart-material-ui';
+import { scaleTime } from 'd3-scale';
+import { ArgumentScale } from '@devexpress/dx-react-chart';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+
+const generateData = (n) => {
+  const ret = [];
+  let y = 0;
+  let ub = 10;
+  let lb = -10;
+  const dt = new Date();
+  for (let i = 0; i < n; i += 1) {
+    dt.setHours(dt.getHours() + 1);
+    y += Math.round(Math.random() * 10 - 5);
+    ret.push({ x: new Date(dt), y, ub, lb });
+  }
+  return ret;
+};
+const data = generateData(250);
+
+const getMode = (zoom, pan) => {
+  if (zoom && pan) {
+    return 'both';
+  }
+  if (zoom && !pan) {
+    return 'zoom';
+  }
+  if (!zoom && pan) {
+    return 'pan';
+  }
+  return 'none';
+};
+
+const chartRootStyle = { marginRight: '20px' };
+
+const ChartRoot = (props) => <Chart.Root {...props} style={chartRootStyle} />;
+
+export default class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data,
+      zoomArgument: true,
+      panArgument: true,
+      zoomValue: false,
+      panValue: false
+    };
+    this.submit = (e) =>
+      this.setState({
+        [e.target.id]: e.target.checked
+      });
+  }
+
+  renderInput(id, label) {
+    const { [id]: checked } = this.state;
+    return (
+      <FormControlLabel
+        control={
+          <Checkbox
+            id={id}
+            checked={checked}
+            onChange={this.submit}
+            value="checkedB"
+            color="primary"
+          />
+        }
+        label={label}
+      />
+    );
+  }
+
+  render() {
+    const {
+      data: chartData,
+      zoomValue,
+      panValue,
+      zoomArgument,
+      panArgument
+    } = this.state;
+    return (
+      <Grid container direction="column">
+        <Grid item xs={12}>
+          <Paper elevation={15}>
+            <Chart data={chartData} rootComponent={ChartRoot}>
+              <ArgumentScale factory={scaleTime} />
+              <ArgumentAxis />
+              <ValueAxis name="IoT" />
+
+              <LineSeries
+                valueField="y"
+                argumentField="x"
+                name="x:Time, y:Vibration(Hz)"
+              />
+              <LineSeries
+                valueField="ub"
+                argumentField="x"
+                name="Upper Bound"
+              />
+              <LineSeries
+                valueField="lb"
+                argumentField="x"
+                name="Lower Bound"
+              />
+
+              <ZoomAndPan
+                interactionWithArguments={getMode(zoomArgument, panArgument)}
+                interactionWithValues={getMode(zoomValue, panValue)}
+              />
+              <Legend position="right" />
+              <Title text="Vibration" />
+            </Chart>
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
+}
