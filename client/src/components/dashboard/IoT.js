a5d9d6bd73fbd8d30d1c6313554cbef655836485
Rupsa Dhar <64538828+rupsadhar@users.noreply.github.com>
2020-07-28 11:07:49 +0530

    removed warning

--- client/src/components/dashboard/IoT.js
+++ client/src/components/dashboard/IoT.js
@@ -14,7 +14,9 @@ const useStateWithLocalStorage = (localStorageKey) => {
   );
   React.useEffect(() => {
     localStorage.setItem(localStorageKey, value);
-  }, [value]);
+  },
+ // eslint-disable-next-line react-hooks/exhaustive-deps
+ [value]);
 
   return [value, setValue];
 };
